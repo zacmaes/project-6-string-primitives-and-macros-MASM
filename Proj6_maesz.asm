4211024cbf915a5eefff7ae050e6433f03e3cf12
zacmaes <zachary.maes@gmail.com>
2022-08-09 17:33:45 -0700

    Add project files.

---
+++ Proj6_maesz.asm
@@ -0,0 +1,32 @@
+TITLE Project Six: String Primitives & Macros     (Proj6_maesz.asm)
+
+; Author: Zachary Maes
+; Last Modified: August 12, 2022
+; OSU email address: maesz@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number: 6                Due Date: August 12, 2022
+; Description: 
+
+; WRITE DESCRIPTION HERE
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
